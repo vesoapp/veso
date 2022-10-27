5e42d6d868b5347cf7ae9c2364a129a779172fc6
Danny Michel <dan@varmag.com>
2022-10-27 12:35:45 -0400

    Revert "Migrate MusicBrainz plugin to MetaBrainz.MusicBrainz"
    This reverts commit 7ad0c9ba24a5f248c2f5b0d89ff096779d22a2b8.

--- MediaBrowser.Providers/Plugins/MusicBrainz/MusicBrainzAlbumExternalId.cs
+++ MediaBrowser.Providers/Plugins/MusicBrainz/MusicBrainzAlbumExternalId.cs
@@ -1,27 +1,28 @@
+#pragma warning disable CS1591
+
 using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Providers;
+using MediaBrowser.Providers.Plugins.MusicBrainz;
 
-namespace MediaBrowser.Providers.Plugins.MusicBrainz;
-
-/// <summary>
-/// MusicBrainz album external id.
-/// </summary>
-public class MusicBrainzAlbumExternalId : IExternalId
+namespace MediaBrowser.Providers.Music
 {
-    /// <inheritdoc />
-    public string ProviderName => "MusicBrainz";
+    public class MusicBrainzAlbumExternalId : IExternalId
+    {
+        /// <inheritdoc />
+        public string ProviderName => "MusicBrainz";
 
-    /// <inheritdoc />
-    public string Key => MetadataProvider.MusicBrainzAlbum.ToString();
+        /// <inheritdoc />
+        public string Key => MetadataProvider.MusicBrainzAlbum.ToString();
 
-    /// <inheritdoc />
-    public ExternalIdMediaType? Type => ExternalIdMediaType.Album;
+        /// <inheritdoc />
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Album;
 
-    /// <inheritdoc />
-    public string? UrlFormatString => Plugin.Instance!.Configuration.Server + "/release/{0}";
+        /// <inheritdoc />
+        public string? UrlFormatString => Plugin.Instance.Configuration.Server + "/release/{0}";
 
-    /// <inheritdoc />
-    public bool Supports(IHasProviderIds item) => item is Audio || item is MusicAlbum;
+        /// <inheritdoc />
+        public bool Supports(IHasProviderIds item) => item is Audio || item is MusicAlbum;
+    }
 }
--- MediaBrowser.Providers/Plugins/MusicBrainz/MusicBrainzReleaseGroupExternalId.cs
+++ MediaBrowser.Providers/Plugins/MusicBrainz/MusicBrainzReleaseGroupExternalId.cs
@@ -1,27 +1,28 @@
+#pragma warning disable CS1591
+
 using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Providers;
+using MediaBrowser.Providers.Plugins.MusicBrainz;
 
-namespace MediaBrowser.Providers.Plugins.MusicBrainz;
-
-/// <summary>
-/// MusicBrainz release group external id.
-/// </summary>
-public class MusicBrainzReleaseGroupExternalId : IExternalId
+namespace MediaBrowser.Providers.Music
 {
-    /// <inheritdoc />
-    public string ProviderName => "MusicBrainz";
+    public class MusicBrainzReleaseGroupExternalId : IExternalId
+    {
+        /// <inheritdoc />
+        public string ProviderName => "MusicBrainz";
 
-    /// <inheritdoc />
-    public string Key => MetadataProvider.MusicBrainzReleaseGroup.ToString();
+        /// <inheritdoc />
+        public string Key => MetadataProvider.MusicBrainzReleaseGroup.ToString();
 
-    /// <inheritdoc />
-    public ExternalIdMediaType? Type => ExternalIdMediaType.ReleaseGroup;
+        /// <inheritdoc />
+        public ExternalIdMediaType? Type => ExternalIdMediaType.ReleaseGroup;
 
-    /// <inheritdoc />
-    public string? UrlFormatString => Plugin.Instance!.Configuration.Server + "/release-group/{0}";
+        /// <inheritdoc />
+        public string? UrlFormatString => Plugin.Instance.Configuration.Server + "/release-group/{0}";
 
-    /// <inheritdoc />
-    public bool Supports(IHasProviderIds item) => item is Audio or MusicAlbum;
+        /// <inheritdoc />
+        public bool Supports(IHasProviderIds item) => item is Audio || item is MusicAlbum;
+    }
 }
