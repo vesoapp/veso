702aef38d8b833a54272f5576a5b50313800bb59
Danny Michel <dan@varmag.com>
2022-10-27 12:35:45 -0400

    Revert "Migrate MusicBrainz plugin to MetaBrainz.MusicBrainz"
    This reverts commit 7ad0c9ba24a5f248c2f5b0d89ff096779d22a2b8.

--- MediaBrowser.Providers/Plugins/MusicBrainz/MusicBrainzAlbumArtistExternalId.cs
+++ MediaBrowser.Providers/Plugins/MusicBrainz/MusicBrainzAlbumArtistExternalId.cs
@@ -1,27 +1,28 @@
+#pragma warning disable CS1591
+
 using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Providers;
+using MediaBrowser.Providers.Plugins.MusicBrainz;
 
-namespace MediaBrowser.Providers.Plugins.MusicBrainz;
-
-/// <summary>
-/// MusicBrainz album artist external id.
-/// </summary>
-public class MusicBrainzAlbumArtistExternalId : IExternalId
+namespace MediaBrowser.Providers.Music
 {
-    /// <inheritdoc />
-    public string ProviderName => "MusicBrainz";
+    public class MusicBrainzAlbumArtistExternalId : IExternalId
+    {
+        /// <inheritdoc />
+        public string ProviderName => "MusicBrainz";
 
-    /// <inheritdoc />
-    public string Key => MetadataProvider.MusicBrainzAlbumArtist.ToString();
+        /// <inheritdoc />
+        public string Key => MetadataProvider.MusicBrainzAlbumArtist.ToString();
 
-    /// <inheritdoc />
-    public ExternalIdMediaType? Type => ExternalIdMediaType.AlbumArtist;
+        /// <inheritdoc />
+        public ExternalIdMediaType? Type => ExternalIdMediaType.AlbumArtist;
 
-    /// <inheritdoc />
-    public string? UrlFormatString => Plugin.Instance!.Configuration.Server + "/artist/{0}";
+        /// <inheritdoc />
+        public string? UrlFormatString => Plugin.Instance.Configuration.Server + "/artist/{0}";
 
-    /// <inheritdoc />
-    public bool Supports(IHasProviderIds item) => item is Audio;
+        /// <inheritdoc />
+        public bool Supports(IHasProviderIds item) => item is Audio;
+    }
 }
